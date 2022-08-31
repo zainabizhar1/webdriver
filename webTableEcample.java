96ed970576ac5045c767c3c5589e4942c0164817
zainabizhar02 <zainabizhar02@gmail.com>
2022-08-31 21:39:48 +0530

    commit

--- webTableEcample.java
+++ webTableEcample.java
@@ -8,53 +8,60 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class webTableEcample {
+public class WebTableEcample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-WebDriver driver =new ChromeDriver();
+		
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://www.w3schools.com/html/html_tables.asp");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
-	
-		List<WebElement> Rows = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr"));
-		System.out.println("total no of row are:" + Rows.size());
-		List<WebElement> Columns = driver.findElements(By.xpath("//table[@id='customers']//th"));
-		System.out.println("total no of colums is " + Columns.size());
-		WebElement data = driver.findElement(By.xpath("//table[@id='customers']//tr[4]/td[1]"));
-		System.out.println("The Required data is:" + data.getText());
 		
-		for (WebElement row:Rows) {
-			List<WebElement> cells = row.findElements(By.tagName("td"));
-			for(WebElement cell:cells) {
-				System.out.print(cell.getText()+ " ");
-				
-				
-			}
-			System.out.println();
+		List<WebElement> Rows = driver.findElements(By.xpath("//table[@id='customers']/tbody/tr"));
+		System.out.println("Total num of rows are " + Rows.size());
+		
+		
+		List<WebElement> Columns = driver.findElements(By.xpath("//table[@id='customers']//th"));
+		System.out.println("Total num of columns are " + Columns.size());
+		
+		WebElement Data = driver.findElement(By.xpath("//table[@id='customers']/tbody/tr[4]/td[1]"));
+		System.out.println("The required data is " + Data.getText());
+		
+		for(WebElement row: Rows) {
+			
+			 List<WebElement> AllCell = row.findElements(By.tagName("td"));
+			 
+			 for(WebElement cell: AllCell) {
+				 
+				 System.out.print(cell.getText() + "\t");
+			 }
+			 
+			 System.out.println();
+			
 		}
 		
 		
-		// Nested for loops
-
-//		for(int row=1; row<=Rows.size();row++) {
-//			
-//			for(int col=1;col<=Columns.size();col++) {
-//				
-//				WebElement cell = driver.findElement(By.xpath("//table[@id='customers']/tbody/tr[" + row + "]/*[" + col + "]"));
-//				System.out.print(cell.getText() + "\t");
-//			}
-//			
-//			System.out.println();
-//		}
-
-
+		for(int row=1; row<=Rows.size();row++) {
+			
+			for(int col=1;col<=Columns.size();col++) {
 				
+				WebElement cell = driver.findElement(By.xpath("//table[@id='customers']/tbody/tr[" + row + "]/*[" + col + "]"));
+				System.out.print(cell.getText() + "\t");
+			}
+			
+			System.out.println();
+		}
+
+		String Company = "Magazzini Alimentari Riuniti";
 		
-		
+		String Xpath = "//td[text()='" + Company + "']/following-sibling::td[2]";
+		WebElement Country = driver.findElement(By.xpath(Xpath));
+		System.out.println("The country is " + Country.getText());
 
 	}
 
-	}
 }
